86beea8e8ad5267c7c0e6a889c04b70cfaf0fa0b
AlongeNet <alongenet32@mail.ru>
2019-05-17 15:11:38 +0300

    Remove image and change path to image tree.jpg for imwrite

--- motion_blur.py
+++ motion_blur.py
@@ -2,18 +2,19 @@ import cv2
 import argparse
 import numpy as np 
 
-
+'''
 #handle cmd arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
-
+'''
+path = 'tree.jpg'
 #Normal window 
 cv2.namedWindow("Motion Blur", cv2.WINDOW_NORMAL)
 #cv2.resizeWindow('0', 600, 600)
 
 #open image 
-image = cv2.imread(args["image"])
+image = cv2.imread(path)
 
 size = 15
 
@@ -23,6 +24,8 @@ kernel_motion_blur = kernel_motion_blur / size
 
 img_output = cv2.filter2D(image, -1, kernel_motion_blur)
 
+#Write to file 
+cv2.imwrite(path, img_output)
 #show an image
 cv2.imshow('Motion Blur', img_output)
 cv2.waitKey(0)
